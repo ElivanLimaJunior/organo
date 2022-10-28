a452cf66e16cc0d573db1aeb05768ef75ddc613d
ElivanLimaJunior <85586631+ElivanLimaJunior@users.noreply.github.com>
2022-10-28 20:44:02 -0300

    Criando Favorito
    Finalizando o projeto adicionando um coração para favorita o colaborador.

--- src/componentes/Colaborador/index.jsx
+++ src/componentes/Colaborador/index.jsx
@@ -1,7 +1,16 @@
-import { HiOutlineTrash } from 'react-icons/hi'
+import { HiOutlineTrash, HiOutlineHeart, HiHeart } from 'react-icons/hi'
 import './colaborador.css'
 
-const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar, aoFavoritar }) => {
+    function favoritar () {
+        aoFavoritar(colaborador.id);
+    }
+
+    const propsfavorito = {
+        size: 25,
+        onClick: favoritar
+    }
+
     return (<div className="colaborador">
         <HiOutlineTrash 
             size={25} 
@@ -14,6 +23,11 @@ const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
         <div className="rodape">
             <h4>{colaborador.nome}</h4>
             <h5>{colaborador.cargo}</h5>
+            <div className='favoritar'>
+                {colaborador.favorito 
+                ? <HiHeart {...propsfavorito} color='#ff0000' /> 
+                : <HiOutlineHeart  {...propsfavorito}/> }
+            </div>
         </div>
     </div>)
 }
